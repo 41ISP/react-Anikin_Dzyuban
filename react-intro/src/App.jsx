e99626ce1a69cac5ddc4739f254dbcedaf3955b1
TTulenchick <dzars.ttulenchick@gmail.com>
2026-09-02 09:03:12 +0000

    123

---
+++ react-intro/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  const username = "TTulenchick"
+ return (
+  <>
+  <h1>С подключением, {username}</h1>
+  <div>
+    <button>-</button>
+    <span>0</span>
+    <button>+</button>
+    </div>
+    </>
+ )
+}
+
+export default App 
+
